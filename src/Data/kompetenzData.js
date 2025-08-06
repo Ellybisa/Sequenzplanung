f631eaf501e38384e251a8d791637a4819e14a46
Ellybisa <benjamin_aschoff@web.de>
2025-08-06 16:46:06 +0200

    Wissensbestände und Kompetenzen in eigene Dateien ausgelagert

---
+++ src/Data/kompetenzData.js
@@ -0,0 +1,98 @@
+export const INHALTSANGABE = [
+  {
+    titel: "SINNABSCHNITTE (INKL. LÄNGE)",
+    jahrgaenge: {
+      "5/6" : [
+        "Sinnabschnitte als individuelle Gliederungsteile verstehen",
+        "Selektionsmechanismen ausprobieren (wie finde ich eine angemessene Anzahl an Sinnabschnitten? z.B. an Figuren oder an Strophen orientiert)"
+      ],
+      "7/8" : [
+        "Sinnabschnitte für Verschriftlichung nutzen können (insbesondere für Inhaltsangabe)",
+        "Anzahlsetzung verstehen (ca. 3-5)"
+      ],
+      "9/10" : [
+        "Verständnis für verschiedene Funktionen im Aufsatz verstehen",
+        "Selektion auch bei variabler Textlänge angemessen einsetzen"
+      ]
+    }
+  },
+  {
+    titel: "GENERALISIERUNG",
+    jahrgaenge: {
+      "5/6" : [
+        "Fokus auf Kürze und Präzision",
+        "Sinnabschnitte im Rahmen des Gesamttextes verallgemeinern"
+      ],
+      "7/8" : [
+        "Sinnabschnitte im Rahmen des Gesamttextes verallgemeinern"
+      ],
+      "9/10" : [
+        "Verallgemeinerung der Sinnabschnitte und Funktionen"
+      ]
+    }
+  },
+  {
+    titel: "KONKRETISIERUNG",
+    jahrgaenge: {
+      "5/6" : [
+        "W-Fragen kennen und ansetzen"
+      ],
+      "7/8" : [
+        "Reduktion von konkreten Informationen",
+        "Inbezugsetzen zur Generalisierung"
+      ],
+      "9/10" : [
+        "+ eindeutiger Zusammenhang angesetzten Funktion"
+      ]
+    }
+  },
+  {
+    titel: "VERWENDUNG EIGENER WORTWAHL",
+    jahrgaenge: {
+      "5/6" : [
+        "Wortschatz ausbauen (Fokus auf Verben, Substantive, Adjektive",
+        "Arbeit mit Synonymen"
+      ],
+      "7/8" : [
+        "Vertiefung Wortschatzarbeit (besonders Fachsprache)",
+        "Sprachstil des Textes verstehen und den Anforderungen der Aufgabenstellung entsprechend schreiben"
+      ],
+      "9/10" : [
+        "Vertiefung Wortschatz (bes. wissenschaftliche Terminologie)",
+        "Sprachstil des Textes verstehen und in angemessener Art und Weise innerhalb der eigenen Sprache professionell umsetzen (z.B. Einsatz direkter Zitate)"
+      ]
+    }
+  },
+];
+
+export const SCHLUSSTEIL = [
+  {
+    titel: "ZUSAMMENFASSUNG",
+    jahrgaenge: {
+      "5/6" : [
+        "Zusammenfassung des Inhalts ohne gehäufte Wiederholung",
+        "keine Beurteilung, keine neuen inhaltlichen Ergebnisse"
+      ],
+      "7/8" : [
+        "Eigene Erkenntnisse verallgemeinern (Zsf.)"
+      ],
+      "9/10" : [
+        "Bezug zum Akzent der Hinführung und/ der Hypothese (roter Faden)"
+      ]
+    }
+  },
+  {
+    titel: "SCHLUSSFOLGERUNG",
+    jahrgaenge: {
+      "5/6" : [
+        "Schlussfolgerung als angeleitet",
+      ],
+      "7/8" : [
+        "Ansaätze einer sinnvollen Schlussfolgerung",
+      ],
+      "9/10" : [
+        "Schlussfolgerung in Bezug eines eigenen Akzentes (z.B. Aktualisierung, Historisierung, Politisierung)"
+      ]
+    }
+  }
+];
